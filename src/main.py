c06062a7f1f2f4fe59e74e0241e08313d5103b49
yongj.zhuang <yongj.zhuang@outlook.com>
2023-03-24 10:19:37 +0800

    Impl websocket connection & query execution

--- src/main.py
+++ src/main.py
@@ -1,16 +1,43 @@
-import json
-import urllib.parse
 import util
 import getpass
 
-csrf = 'DfDAGsq68lj08TuVEjlkgPNQsESYXwCTmOnrOjoNd6NQxgEfvfCbYCYzOMwq3dus'
-
-base = 'http://localhost:8081/omnidb'
-
+csrf = 'NqZm7ziofRZchMRZfrg9LXaQ1TdJWVcqn9XdSpKi8GsJZUlmeZTN8DNzY7ZjmAdl'
+uname = ''
+pw = ''
+host = ""
 
 if __name__ == "__main__":
-    uname = input("Enter Username: ")
-    pw = getpass.getpass(f"Enter Password for '{uname}':")
+    if not uname:
+        uname = input("Enter Username: ")
+    if not pw:
+        pw = getpass.getpass(f"Enter Password for '{uname}':")
 
     # login
-    sessionid = util.login(csrf, base, uname, pw)
+    sh = util.login(csrf, host, uname, pw)
+    print(f"cookie: {sh.cookie}")
+
+    # connect websocket
+    ws = util.connect_ws(sh, host)
+
+    # change active database
+    # TODO remove hard-coded values
+    util.change_active_database(sh, 1, "conn_tabs_tab4", "")
+
+    # first message
+    # TODO remove hard-coded values
+    util.send_recv(ws, f'{{"v_code":0,"v_context_code":0,"v_error":false,"v_data":"{sh.sessionid}"}}')
+
+    # execute queries
+    # TODO remove hard-coded values
+    print("Entering sql query interactive mode, enter 'quit()' to exit")
+    while True: 
+        sql = input("Enter Query: ").strip()
+        if sql == 'quit()': break
+        if sql == "": continue
+
+        msg = f'{{"v_code":1,"v_context_code":3,"v_error":false,"v_data":{{"v_sql_cmd":"{sql}","v_sql_save":"{sql}","v_cmd_type":null,"v_db_index":1,"v_conn_tab_id":"conn_tabs_tab4","v_tab_id":"conn_tabs_tab4_tabs_tab1","v_tab_db_id":57,"v_mode":0,"v_all_data":false,"v_log_query":true,"v_tab_title":"Query","v_autocommit":true}}}}'
+        util.send_recv(ws, msg, 2)
+
+    # disconnect websocket
+    ws.close()
+    print("Websocket disconnected")
--- src/util.py
+++ src/util.py
@@ -1,11 +1,31 @@
 import requests
 import json
+from websocket import create_connection, WebSocket
 
-session = requests.Session()
+DEFAULT_PROTOCOL = "https://"
+session = requests.Session() # reuse connection
+
+class OSession:
+    def __init__(self, set_cookie: str, csrf_token: str, host: str, protocol: str):
+        self.sessionid = ""
+        self.host = host 
+        self.protocol = protocol
+        self.csrf = csrf_token
+
+        for v in set_cookie.split(';'):
+            kv = v.strip().split('=')
+            if kv[0] == 'omnidb_sessionid':
+                self.sessionid = kv[1]
+                break
+
+        self.cookie = f"omnidb_sessionid={self.sessionid}; omnidb_csrftoken={csrf_token}"
 
 
-def login(csrf: str, base: str, username: str, password: str) -> str:
-    resp: requests.Response = session.post(base + '/sign_in/', data={
+def login(csrf: str, host: str, username: str, password: str, protocol: str = DEFAULT_PROTOCOL) -> "OSession":
+    url = protocol + host + '/sign_in/'
+    print(f"Trying to login, url: {url}")
+
+    resp: requests.Response = session.post(url, data={
         'data': json.dumps({'p_username': username, 'p_pwd': password})
     }, headers={
         'cookie': f'omnidb_csrftoken={csrf}',
@@ -17,12 +37,50 @@ def login(csrf: str, base: str, username: str, password: str) -> str:
             f"Login failed, code: {resp.status_code}, msg: {resp.text}, headers: {resp.headers}")
 
     # omnidb_sessionid
-    setcookie = resp.headers['Set-Cookie']
-    splited = setcookie.split(';')
-    for i in range(len(splited)):
-        kv = splited[i].strip().split('=')
-        if kv[0] == 'omnidb_sessionid':
-            sessionid = kv[1]
-            break
+    if not 'Set-Cookie' in resp.headers:
+        raise ValueError(
+            f"Login failed, unable to find set-cookie, make sure that you are not already signed-in in your browser, code: {resp.status_code}, msg: {resp.text}, headers: {resp.headers}")
 
-    print(f"omnidb_sessionid={sessionid}")
+    sh = OSession(resp.headers['Set-Cookie'], csrf, host, protocol)
+    print(f"omnidb_sessionid={sh.sessionid}")
+    if not sh.sessionid:
+        raise ValueError(
+            f"Login failed, unable to find omnidb_sessionid, make sure that you are not already signed-in in your browser, code: {resp.status_code}, msg: {resp.text}, headers: {resp.headers}")
+
+    return sh
+
+
+def send_recv(ws: WebSocket, payload, wait_recv_times=1):
+    ws.send(payload)
+    print(f"ws sent: '{payload}'")
+    for i in range(wait_recv_times):
+        r = ws.recv()
+        print(f"ws received: '{r}'")
+
+
+def connect_ws(sh: OSession, host: str, protocol: str = "wss://") -> WebSocket:
+    url = protocol + host
+    if not url.endswith("/"):
+        url += "/"
+    url += "wss"
+    print(f"Connecting to websocket server, url: {url}")
+    ws = create_connection(
+        url, headers=["Upgrade: websocket"], cookie=sh.cookie)
+    print(f"Successfully connected to websocket server")
+    return ws
+
+
+def change_active_database(sh: OSession, p_database_index, p_tab_id, p_database):
+    url = sh.protocol + sh.host + '/change_active_database/'
+    print(f"Changing active database, url: '{url}'")
+
+    resp: requests.Response = session.post(url, data={
+        'data': json.dumps({'p_database_index': p_database_index, 'p_tab_id': p_tab_id, "p_database": p_database})
+    }, headers={
+        'cookie': sh.cookie,
+        'x-csrftoken': sh.csrf,
+        'x-requested-with': 'XMLHttpRequest'
+    })
+    if resp.status_code != 200:
+        raise ValueError(
+            f"Change active database failed, code: {resp.status_code}, msg: {resp.text}, headers: {resp.headers}")
